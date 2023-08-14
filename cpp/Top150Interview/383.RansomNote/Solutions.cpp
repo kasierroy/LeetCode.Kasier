fce676853d8fe1722f5f7dc53ffb5a15b92c204a
Kasier Roy S. Narandan <kasierroy@gmail.com>
2023-08-14 12:24:23 +0900

    Top15Interview: Add 383.RansomNote

---
+++ cpp/Top150Interview/383.RansomNote/Solutions.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> mp;
+        for (int i = 0; i < magazine.length(); i ++)
+            mp[magazine[i]]++;
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (mp[ransomNote[i]] > 0)
+                mp[ransomNote[i]]--;
+            else
+                return false;
+        }
+
+        return true;
+    }
+};
