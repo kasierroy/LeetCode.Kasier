bbe1c1ecdcc6def1420aad9db0bb438ff677cdd4
Kasier Roy S. Narandan <kasierroy@gmail.com>
2023-08-15 17:16:34 +0900

    Top150Interview: Add 136.SingleNumber

---
+++ cpp/Top150Interview/136.SingleNumber/Solutions.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+            auto itr = mp.find(nums[i]);
+            if (itr->second >= 2)
+                mp.erase(itr);
+        }
+
+        return mp.begin()->first;
+    }
+};
